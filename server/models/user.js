c912a7ebd87826da9b74ff002decae1bd86320ac
Suraj Chandgude <surajchandgude0304@gmail.com>
2018-10-09 15:56:55 +0530

    Added POST/ users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -81,15 +81,35 @@ var UserSchema = new mongoose.Schema( {
         if (user.isModified('password')) {
             bcrypt.genSalt(10, (err, salt) => {
                 bcrypt.hash(user.password, salt, (err, hash) => {
-                    user.password = hash;
+                    user.password = hash; 
                     next();
                 });
             });
-            
+
     } else {
         next();
     }
 });
+
+    UserSchema.statics.findByCredentials = function (email, password) {
+        var User = this;
+
+        return User.findOne({email}).then((user) => {
+            if(!user){
+                return Promise.reject();
+            }
+
+            return new Promise((resolve, reject) => {
+                bcrypt.compare(password, user.password, (err, res) => {
+                     if (res) {     
+                         resolve(user); 
+                     } else {
+                         reject();
+                     }
+                }); 
+            });
+        });
+    }
 
 var User = mongoose.model('User', UserSchema);
      
--- server/server.js
+++ server/server.js
@@ -134,6 +134,18 @@ app.get('/users/me', authenticate,  (req, res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+    
+        var body = _.pick(req.body, ['email', 'password'])        
+        User.findByCredentials(body.email, body.password).then((user) => {
+               return user.generateAuthToken().then((token) => {
+                    res.header('x-path',token).send(user);    
+                });
+        }).catch((e) => {
+            res.status(400).send();
+        });
+});
+
 app.listen(3000, () => {
     console.log(`Started on port ${port}`);
 });
